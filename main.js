b26a9c39d0c38ea35a4d10deadbac1e65cfaa364
eugene-sakwa <the.ardent.arian@gmail.com>
2025-01-17 23:54:29 +0100

    Construct the main function for the app

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log ( 'Hello world');
+process.argv
+
+const main = () => {
+  if(process.argv.length < 3){
+    console.log(`Please pass in a valid url`);
+    process.exit(1);
+  }
+  if(process.argv.length > 3){
+    console.log(`You have provide excess arguments `);
+    process.exit(1);
+  }
+const baseURL = process.argv[2];
+  console.log(`Starting crawl of ${baseURL}...`)
+};
+
+main();
