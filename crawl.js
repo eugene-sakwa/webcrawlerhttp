a23a26f208d419d76df52309cec557992a311a1b
eugene-sakwa <the.ardent.arian@gmail.com>
2025-01-17 23:36:56 +0100

    getURLfromHTML invalid function and tests

--- crawl.js
+++ crawl.js
@@ -6,9 +6,19 @@ const getURLsFromHTML = (htmlBody, baseURL) => {
   const linkElements = dom.window.document.querySelectorAll("a");
   for (let linkElement of linkElements) {
     if (linkElement.href.slice(0, 1) === "/") {
-      urls.push(`${baseURL}${linkElement.href}`);
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with relative url: ${err.message}`);
+      }
     } else {
-      urls.push(linkElement.href);
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`Error with absolute url: ${err.message}`);
+      }
     }
   }
   return urls;
